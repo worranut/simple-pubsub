40c361643205a7854396c8d4cb84686dc703c6fb
Worranut Duangkaew <worranut.du@gmail.com>
2023-09-10 01:20:22 +0700

    Implement class PublishSubscribeService. Implement machineId(), type(), getRefillQuantity() at class MachineRefillEvent. Fix bug handle() at class MachineSaleSubscriber. Implement class MachineRefillSubscriber.

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 Instructions
+
 1. Build the Publish-Subscribe mechanism. Allow ISubscriber objects to register against an concrete IPublishSubscribeService object for an event type. Implement the publish method so that when a publish event occurs, all subscribers of that the event type published will have a chance to handle the event. The subscribers should be working off a shared array of Machine objects, mutating them depending on the event received.
 2. Now add the method 'unsubscribe' on IPublishSubscribeService to allow handlers to unsubscribe from events. You may change the existing method signatures.
 3. Implement MachineRefillSubscriber. It will increase the stock quantity of the machine.
-4. Let's add some new behaviour. If a machine stock levels drops below 3 a new Event, LowStockWarningEvent should be generated. When the stock levels hits 3 or above (because of a MachineRefillEvent), a StockLevelOkEvent should be generated.  For each machine, LowStockWarningEvent or StockLevelOkEvent should only fire one time when crossing the threshold of 3. You may want to introduce new subscribers (e.g. a new subscriber called StockWarningSubscriber). In fact you may change anything as long as the task is performed and you can justify your reasonings. Remember subscribers should be notified in the order that the events occured.
+4. Let's add some new behaviour. If a machine stock levels drops below 3 a new Event, LowStockWarningEvent should be generated. When the stock levels hits 3 or above (because of a MachineRefillEvent), a StockLevelOkEvent should be generated. For each machine, LowStockWarningEvent or StockLevelOkEvent should only fire one time when crossing the threshold of 3. You may want to introduce new subscribers (e.g. a new subscriber called StockWarningSubscriber). In fact you may change anything as long as the task is performed and you can justify your reasonings. Remember subscribers should be notified in the order that the events occured.
 
 Your program should now allow you to create ISubscriber objects, register them using your IPublishSubscribService implementation. You can then create IEvent objects and call your IPublishSubscribService's implementations .publish() method. All handlers subscribed should have their 'handle' methods invoked.
 
@@ -12,9 +13,15 @@ Note II: If a subscriber subscribes after an event has already been published an
 
 You may make any changes to this codebase as long as you ultimately build a Pub-Sub application capable of handling the existing machine sale and refill events.
 
-Additional credit for using useful abstractions that add to the codebase. This is fairly general, and is here for you to demonstrate your knowledge. Some examples might be 
+Additional credit for using useful abstractions that add to the codebase. This is fairly general, and is here for you to demonstrate your knowledge. Some examples might be
 (i) using a repository for storing Machines and injecting them into events subscriber
 (ii) using advanced functional concepts like an Maybe
 (iii) and more
 
 Please share your work using a GitHub repository with @proftom.
+
+==============================================================
+
+Run this program use command:
+
+"npx ts-node app.ts"
--- app.ts
+++ app.ts
@@ -9,107 +9,159 @@ interface ISubscriber {
 }
 
 interface IPublishSubscribeService {
-  publish (event: IEvent): void;
-  subscribe (type: string, handler: ISubscriber): void;
+  publish(event: IEvent): void;
+  subscribe(type: string, handler: ISubscriber): void;
   // unsubscribe ( /* Question 2 - build this feature */ );
 }
 
-
 // implementations
 class MachineSaleEvent implements IEvent {
-  constructor(private readonly _sold: number, private readonly _machineId: string) {}
+  constructor(
+    private readonly _sold: number,
+    private readonly _machineId: string
+  ) {}
 
   machineId(): string {
     return this._machineId;
   }
 
   getSoldQuantity(): number {
-    return this._sold
+    return this._sold;
   }
 
   type(): string {
-    return 'sale';
+    return "sale";
   }
 }
 
 class MachineRefillEvent implements IEvent {
-  constructor(private readonly _refill: number, private readonly _machineId: string) {}
+  constructor(
+    private readonly _refill: number,
+    private readonly _machineId: string
+  ) {}
 
   machineId(): string {
-    throw new Error("Method not implemented.");
+    return this._machineId;
+  }
+
+  getRefillQuantity(): number {
+    return this._refill;
   }
 
   type(): string {
-    throw new Error("Method not implemented.");
+    return "refill";
   }
 }
 
 class MachineSaleSubscriber implements ISubscriber {
   public machines: Machine[];
 
-  constructor (machines: Machine[]) {
-    this.machines = machines; 
+  constructor(machines: Machine[]) {
+    this.machines = machines;
   }
 
   handle(event: MachineSaleEvent): void {
-    this.machines[2].stockLevel -= event.getSoldQuantity();
+    const machine = this.machines.find((m) => m.id === event.machineId());
+    if (machine) {
+      machine.stockLevel -= event.getSoldQuantity();
+    }
   }
 }
 
 class MachineRefillSubscriber implements ISubscriber {
-  handle(event: IEvent): void {
-    throw new Error("Method not implemented.");
+  public machines: Machine[];
+
+  constructor(machines: Machine[]) {
+    this.machines = machines;
+  }
+
+  handle(event: MachineRefillEvent): void {
+    const machine = this.machines.find((m) => m.id === event.machineId());
+    if (machine) {
+      machine.stockLevel += event.getRefillQuantity();
+    }
   }
 }
 
+// Publish-Subscribe Service Implementation
+class PublishSubscribeService implements IPublishSubscribeService {
+  private subscribers: Record<string, ISubscriber[]> = {};
+
+  subscribe = (type: string, handler: ISubscriber): void => {
+    if (!this.subscribers[type]) {
+      this.subscribers[type] = [];
+    }
+    this.subscribers[type].push(handler);
+  };
+
+  publish = (event: IEvent): void => {
+    const type = event.type();
+    console.log(event);
+
+    if (this.subscribers[type]) {
+      this.subscribers[type].forEach((subscriber) => {
+        subscriber.handle(event);
+        console.log(subscriber);
+        console.log("======================");
+      });
+    }
+  };
+}
 
 // objects
 class Machine {
   public stockLevel = 10;
   public id: string;
 
-  constructor (id: string) {
+  constructor(id: string) {
     this.id = id;
   }
 }
-
 
 // helpers
 const randomMachine = (): string => {
   const random = Math.random() * 3;
   if (random < 1) {
-    return '001';
+    return "001";
   } else if (random < 2) {
-    return '002';
+    return "002";
   }
-  return '003';
-
-}
+  return "003";
+};
 
 const eventGenerator = (): IEvent => {
   const random = Math.random();
   if (random < 0.5) {
     const saleQty = Math.random() < 0.5 ? 1 : 2; // 1 or 2
     return new MachineSaleEvent(saleQty, randomMachine());
-  } 
+  }
   const refillQty = Math.random() < 0.5 ? 3 : 5; // 3 or 5
   return new MachineRefillEvent(refillQty, randomMachine());
-}
-
+};
 
 // program
+const pubSubService: IPublishSubscribeService = new PublishSubscribeService();
+
 (async () => {
   // create 3 machines with a quantity of 10 stock
-  const machines: Machine[] = [ new Machine('001'), new Machine('002'), new Machine('003') ];
+  const machines: Machine[] = [
+    new Machine("001"),
+    new Machine("002"),
+    new Machine("003"),
+  ];
 
   // create a machine sale event subscriber. inject the machines (all subscribers should do this)
   const saleSubscriber = new MachineSaleSubscriber(machines);
+  const refillSubscriber = new MachineRefillSubscriber(machines);
 
   // create the PubSub service
-  const pubSubService: IPublishSubscribeService = null as unknown as IPublishSubscribeService; // implement and fix this
+
+  // subscribe subscribers to events
+  pubSubService.subscribe("sale", saleSubscriber);
+  pubSubService.subscribe("refill", refillSubscriber);
 
   // create 5 random events
-  const events = [1,2,3,4,5].map(i => eventGenerator());
+  const events = [1, 2, 3, 4, 5].map((i) => eventGenerator());
 
   // publish the events
   events.map(pubSubService.publish);
